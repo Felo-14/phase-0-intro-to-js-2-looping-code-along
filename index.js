8aa60dd682a6bd8795b48b996a44dd48705299c7
felix mwendwa <felix.mwendwa@student.moringaschool.com>
2022-09-01 08:33:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(stringName, eventName) {
+    let message = []
+    for (let i = 0; i < stringName.length; i++) {
+        message.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return message;
+}
+
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number-=1;
+    }
+}
